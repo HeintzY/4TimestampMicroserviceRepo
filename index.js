ecc3fe2fb96185687839cb176a64e9d507a94e6e
HeintzY <heintzymail@testmail.com>
2025-05-01 10:23:53 +0200

    feat: add finished project

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,7 +21,28 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
+});
+
+
+// Project Solution:
+app.get("/api", function (req, res) {
+  res.json({ unix: new Date().getTime(), utc: new Date().toUTCString() });
+});
+
+app.get("/api/:date?", function (req, res) {
+  let date_string = req.params.date;
+  const reg = new RegExp('^[0-9]+$');
+  if (new Date(date_string).getTime() && new Date(date_string).toUTCString()) {
+    res.json({ unix: new Date(date_string).getTime(), utc: new Date(date_string).toUTCString() });
+  }
+  else if (reg.test(date_string)) {
+    date_string = parseInt(date_string);
+    res.json({ unix: new Date(date_string).getTime(), utc: new Date(date_string).toUTCString() });
+  }
+  if (!new Date(date_string).getTime() || !new Date(date_string).toUTCString()) {
+    res.json({ error: "Invalid Date" });
+  }
 });
 
 
